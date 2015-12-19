9f9aa63fc2a3d846074f6b925e4772b356dab900
Carl Eriksson <carl.eriksson@funka.com>
2015-12-19 11:51:24 +0100

    Added files

---
+++ LetsBet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LetsBet.Startup))]
+namespace LetsBet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
